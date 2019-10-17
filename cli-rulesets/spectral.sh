e1e424e98c1e885ad5991074fd39f82c391a991f
lauretar <arnaud.lauret@natixis.com>
2019-10-17 10:39:07 +0200

    Updated ruleset with new rules and switched to Spectral v4 format

--- cli-rulesets/spectral.sh
+++ cli-rulesets/spectral.sh
@@ -1,4 +1,4 @@
 spec=$1
 rules=$2
 
-spectral lint $1 -r $2 -f json -o result.json; cat result.json | jq "map({path: .path | join(\".\"), line: .range.start.line, code: .code, message: .message})";rm result.json
+spectral lint $1 -r $2 -f json -q | jq "map({path: .path | join(\".\"), line: .range.start.line, code: .code, message: .message})"
