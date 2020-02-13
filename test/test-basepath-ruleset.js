4e0efb6de5c7f4df721d313abfc77e04b7ac938e
lauretar <arnaud.lauret@natixis.com>
2020-02-13 14:56:41 +0100

    basepath: now handles oas2 and oas3

--- test/test-basepath-ruleset.js
+++ test/test-basepath-ruleset.js
@@ -15,16 +15,53 @@ describe('basepath', function () {
     }
   })
 
-  describe('basepath-defined', function () {
-    it('should not return an error if basepath is defined', async function () {
+  describe('basepath-defined-oas2', function () {
+    it('should ignore missing basepath in oas3 document', async function () {
       const document = {
+        openapi: '3.0'
+      }
+      await spectralTestWrapper.runAndCheckNoError(document)
+    })
+
+    it('should return no error if basepath is defined in oas2 document', async function () {
+      const document = {
+        swagger: '2.0',
         basePath: '/someBasePath'
       }
       await spectralTestWrapper.runAndCheckNoError(document)
     })
 
-    it('should return an error if basepath is not defined', async function () {
-      const document = {}
+    it('should return an error if basepath is not defined in oas2 document', async function () {
+      const document = {
+        swagger: '2.0'
+      }
+      const errorPath = []
+      const errorSeverity = SEVERITY.error
+
+      await spectralTestWrapper.runAndCheckExpectedError(document, rule(this), errorPath, errorSeverity)
+    })
+  })
+
+  describe('basepath-defined-oas3', function () {
+    it('should ignore missing servers[0].url in oas2 document', async function () {
+      const document = {
+        swagger: '2.0'
+      }
+      await spectralTestWrapper.runAndCheckNoError(document)
+    })
+
+    it('should return no error if servers[0].url is defined in oas3 document', async function () {
+      const document = {
+        openapi: '3.0',
+        servers: [{ url: '/someBasePath' }]
+      }
+      await spectralTestWrapper.runAndCheckNoError(document)
+    })
+
+    it('should return an error if servers[0].url is not defined in oas3 document', async function () {
+      const document = {
+        openapi: '3.0'
+      }
       const errorPath = []
       const errorSeverity = SEVERITY.error
 
@@ -33,18 +70,70 @@ describe('basepath', function () {
   })
 
   describe('basepath-valid-structure', function () {
-    it('should not return an error if basepath has a valid structure', async function () {
+    it('should return no error if oas2 basepath is /name/v2', async function () {
       const document = {
-        basePath: '/someName/v2'
+        basePath: '/name/v2'
       }
       await spectralTestWrapper.runAndCheckNoError(document)
     })
 
-    it('should return an error if basepath has not a valid structure', async function () {
+    it('should return no error if oas2 basepath is /some-name/v22', async function () {
       const document = {
-        basePath: '/someBasePath'
+        basePath: '/some-name/v22'
+      }
+      await spectralTestWrapper.runAndCheckNoError(document)
+    })
+
+    it('should return no error if oas2 basepath is /some_name/v22', async function () {
+      const document = {
+        basePath: '/some_name/v22'
+      }
+      await spectralTestWrapper.runAndCheckNoError(document)
+    })
+
+    it('should return no error if oas2 basepath is /someName/v1', async function () {
+      const document = {
+        basePath: '/someName/v1'
+      }
+      await spectralTestWrapper.runAndCheckNoError(document)
+    })
+
+    it('should return an error if oas2 basepath is /nameWithoutVersion', async function () {
+      const document = {
+        basePath: '/nameWithoutVersion'
       }
       const errorPath = ['basePath']
+      const errorSeverity = SEVERITY.error
+
+      await spectralTestWrapper.runAndCheckExpectedError(document, rule(this), errorPath, errorSeverity)
+    })
+
+    it('should return an error if oas2 basepath is /v1', async function () {
+      const document = {
+        basePath: '/v1'
+      }
+      const errorPath = ['basePath']
+      const errorSeverity = SEVERITY.error
+
+      await spectralTestWrapper.runAndCheckExpectedError(document, rule(this), errorPath, errorSeverity)
+    })
+
+    it('should return no error if oas3 server url has a valid structure (no need to check all regex cases again)', async function () {
+      const document = {
+        servers: [
+          { url: '/someName/v1' }
+        ]
+      }
+      await spectralTestWrapper.runAndCheckNoError(document)
+    })
+
+    it('should return an error if oas3 server url has an invalid structure (no need to check all regex cases again)', async function () {
+      const document = {
+        servers: [
+          { url: '/nameWithoutVersion' }
+        ]
+      }
+      const errorPath = ['servers', '0', 'url']
       const errorSeverity = SEVERITY.error
 
       await spectralTestWrapper.runAndCheckExpectedError(document, rule(this), errorPath, errorSeverity)
@@ -52,14 +141,28 @@ describe('basepath', function () {
   })
 
   describe('basepath-lowerCamelCased', function () {
-    it('should not return an error if basepath is lowerCamelCased', async function () {
+    it('should not return no error if oas2 basepath is /lowerCamelCased', async function () {
       const document = {
         basePath: '/lowerCamelCased'
       }
       await spectralTestWrapper.runAndCheckNoError(document)
     })
 
-    it('should return an error if basepath is NotLowerCamelCased', async function () {
+    it('should not return no error if oas2 basepath is /v1', async function () {
+      const document = {
+        basePath: '/v1'
+      }
+      await spectralTestWrapper.runAndCheckNoError(document)
+    })
+
+    it('should not return no error if oas2 basepath is /lowerCamelCased/v1', async function () {
+      const document = {
+        basePath: '/lowerCamelCased/v1'
+      }
+      await spectralTestWrapper.runAndCheckNoError(document)
+    })
+
+    it('should return an error if oas2 basepath is /NotLowerCamelCased', async function () {
       const document = {
         basePath: '/NotLowerCamelCased'
       }
@@ -69,11 +172,43 @@ describe('basepath', function () {
       await spectralTestWrapper.runAndCheckExpectedError(document, rule(this), errorPath, errorSeverity)
     })
 
-    it('should return an error if basepath is notLOWERCamelCased', async function () {
+    it('should return an error if oas2 basepath is /V1', async function () {
       const document = {
-        basePath: '/notLOWERCamelCased'
+        basePath: '/V1'
       }
       const errorPath = ['basePath']
+      const errorSeverity = SEVERITY.error
+
+      await spectralTestWrapper.runAndCheckExpectedError(document, rule(this), errorPath, errorSeverity)
+    })
+
+    it('should return an error if oas2 basepath is /NotLowerCamelCased/V1', async function () {
+      const document = {
+        basePath: '/NotLowerCamelCased/V1'
+      }
+      const errorPath = ['basePath']
+      const errorSeverity = SEVERITY.error
+
+      await spectralTestWrapper.runAndCheckExpectedError(document, rule(this), errorPath, errorSeverity)
+    })
+
+    it('should return no error if oas3 server url is lowerCamelCased (no need to check all regex cases again)', async function () {
+      const document = {
+        servers: [
+          { url: '/lowerCamelCased/v1' }
+        ]
+      }
+
+      await spectralTestWrapper.runAndCheckNoError(document)
+    })
+
+    it('should return an error if oas3 server url is not lowerCamelCased (no need to check all regex cases again)', async function () {
+      const document = {
+        servers: [
+          { url: '/NotLowerCamelCased/V1' }
+        ]
+      }
+      const errorPath = ['servers', '0', 'url']
       const errorSeverity = SEVERITY.error
 
       await spectralTestWrapper.runAndCheckExpectedError(document, rule(this), errorPath, errorSeverity)
@@ -81,14 +216,14 @@ describe('basepath', function () {
   })
 
   describe('basepath-no-trailing-slash', async function () {
-    it('should return no error if basepath has no trailing slash', async function () {
+    it('should return no error if oas2 basepath has no trailing slash', async function () {
       const document = {
         basePath: '/no-trailing-slash'
       }
       await spectralTestWrapper.runAndCheckNoError(document)
     })
 
-    it('should return an error if basepath has a trailing slash', async function () {
+    it('should return an error if oas2 basepath has a trailing slash', async function () {
       const document = {
         basePath: '/forbidden-trailing-slash/'
       }
@@ -98,7 +233,7 @@ describe('basepath', function () {
       await spectralTestWrapper.runAndCheckExpectedError(document, rule(this), errorPath, errorSeverity)
     })
 
-    it('should return an error if basepath is just slash', async function () {
+    it('should return an error if oas2 basepath is just slash', async function () {
       const document = {
         basePath: '/'
       }
@@ -107,21 +242,55 @@ describe('basepath', function () {
 
       await spectralTestWrapper.runAndCheckExpectedError(document, rule(this), errorPath, errorSeverity)
     })
+
+    it('should return no error if oas3 server url has no trailing slash', async function () {
+      const document = {
+        servers: [{ url: '/no-trailing-slash' }]
+      }
+      await spectralTestWrapper.runAndCheckNoError(document)
+    })
+
+    it('should return an error if oas3 server url has a trailing slash (no need to check all regex cases again)', async function () {
+      const document = {
+        servers: [{ url: '/forbidden-trailing-slash/' }]
+      }
+      const errorPath = ['servers', '0', 'url']
+      const errorSeverity = SEVERITY.error
+
+      await spectralTestWrapper.runAndCheckExpectedError(document, rule(this), errorPath, errorSeverity)
+    })
   })
 
   describe('basepath-no-api', function () {
-    it('should return no error if basepath does not contain api', async function () {
+    it('should return no error if oas2 basepath does not contain api', async function () {
       const document = {
         basePath: '/some/path'
       }
       await spectralTestWrapper.runAndCheckNoError(document)
     })
 
-    it('should return an error if basepath contains api', async function () {
+    it('should return an error if oas2 basepath contains api', async function () {
       const document = {
         basePath: '/api/path'
       }
       const errorPath = ['basePath']
+      const errorSeverity = SEVERITY.error
+
+      await spectralTestWrapper.runAndCheckExpectedError(document, rule(this), errorPath, errorSeverity)
+    })
+
+    it('should return no error if oas3 server url does not contain api', async function () {
+      const document = {
+        servers: [{ url: '/some/path' }]
+      }
+      await spectralTestWrapper.runAndCheckNoError(document)
+    })
+
+    it('should return an error if oas3 server url contains api', async function () {
+      const document = {
+        servers: [{ url: '/api/path' }]
+      }
+      const errorPath = ['servers', '0', 'url']
       const errorSeverity = SEVERITY.error
 
       await spectralTestWrapper.runAndCheckExpectedError(document, rule(this), errorPath, errorSeverity)
